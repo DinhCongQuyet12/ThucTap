6ed1681adc2505fd469442f11d760918bebf4273
DinhCongQuyet12 <congquyet16032001@gmail.com>
2023-06-07 00:09:15 +0700

    XemphanJWT

---
+++ JWT/JWT/User.cs
@@ -0,0 +1,10 @@
+﻿namespace JWT
+{
+    public class User
+    {
+        public string Username { set; get; } = string.Empty;
+        public byte[] PasswordHash { set; get; }
+        public byte[] PasswordSalt { set; get; }
+
+    }
+}
