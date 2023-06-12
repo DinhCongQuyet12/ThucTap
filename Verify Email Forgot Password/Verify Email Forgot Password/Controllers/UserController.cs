55d6e53d9200024ad660d51604344cdbd1046d0d
DinhCongQuyet12 <congquyet16032001@gmail.com>
2023-06-12 22:06:45 +0700

    bao cao tt

---
+++ Verify Email Forgot Password/Verify Email Forgot Password/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using VerifyEmailForgotPassword.Data;
+using VerifyEmailForgotPassword.Model;
+
+namespace VerifyEmailForgotPassword.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpPost("register")]
+
+        public async Task<IActionResult> Register(UserRegisterRequest request)
+        {
+            if (_context.Users.Any(u => u.Email == request.Email))
+            {
+                return BadRequest("User already exists");
+            }
+            CreatePasswordHash(request.Password,
+                           out byte[] passwordHash,
+                           out byte[] passwordSalt);
+            var user = new User
+            {
+                Email = request.Email,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                VerificationToken = CreateRandomToken()
+            };
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok("User successfully created");
+        }
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+        private string CreateRandomToken()
+        {
+            return Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
+        }
+    }
+}
